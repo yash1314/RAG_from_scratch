fd2ce2ce515697c2cd8407f3bc83a2c39c9e2544
yash1314 <yashkeshari79@gmail.com>
2024-08-14 17:09:32 +0530

    model prompt change

--- src/model_components/models.py
+++ src/model_components/models.py
@@ -30,8 +30,12 @@ class Model:
     def QA_model(u_input, type, context:str = None):
         try:
             if type=="qa":
-                messages = [{"role": "system", "content": """You are helpful and polite assistant.Below is an instruction that 
-                                                    describes a task. Write a response that appropriately completes the request."""},
+                messages = [{"role": "system", "content": """You help everyone by answering questions, and improve your answers from previous answer in History.
+                                                Don't try to make up an answer, if you don't know just say that you don't know.
+                                                Answer in the same language the question was asked.
+                                                Answer in a way that is easy to understand.
+                                                Do not say "Based on the information you provided, ..." or "I think the answer is...". Just answer the question directly in detail.
+                                                Use only the following pieces of context to answer the question at the end."""},
                             {"role": "user", "content": u_input}]
                 
                 output = Model.load_t2t_model()(messages, max_new_tokens = 384)
