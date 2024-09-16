d2cc9c18d37e526ebdac486515d2092671a67279
yash1314 <yashkeshari79@gmail.com>
2024-09-16 12:11:59 +0530

    logging changes

--- src/pipelines/main_pipe.py
+++ src/pipelines/main_pipe.py
@@ -7,18 +7,14 @@ from src.exception import CustomException
 import sys
 
 def file_handling(file):
-    try : 
-        # store file.
-        DataFile.store_file(file=file, folder_name='artifact')
-
-        # extract data and transform file.
-        PDF_Extract_Transform.transform(file_name=file.name, file_path='artifact', save_path="artifact/data")
-
-        # Perform embedding and similarity search on transformed data.
-        Similarity_Search.embed_data('artifact/data/ext_data.csv')
-
-        logging.info("File successfully handled.")
     
-    except Exception as e:
-        logging.info("Error in file handling pipeline file")
-        raise CustomException(e, sys)
+    # store file.
+    DataFile.store_file(file=file, folder_name='artifact')
+
+    # extract data and transform file.
+    PDF_Extract_Transform.transform(file_name=file.name, file_path='artifact', save_path="artifact/data")
+
+    # Perform embedding and similarity search on transformed data.
+    Similarity_Search.embed_data('artifact/data/ext_data.csv')
+
+    logging.info("File successfully handled.")
