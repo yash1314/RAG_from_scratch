ef7b380de2507aece5fa3cfcc0a1a917d9f163a1
yash1314 <yashkeshari79@gmail.com>
2024-09-19 11:37:56 +0530

    logging changes

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging, os, sys, datetime
 
 LOG_FILE = f"{datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"   # Log File name 
 
-log_dir = os.path.join(os.getcwd(), 'logs', LOG_FILE)     # creates the path for log folder with file_name
+log_dir = os.path.join(os.getcwd(), 'logs')     # creates the path for log folder with file_name
 
 os.makedirs(log_dir, exist_ok=True)   # make dir, if exist prevent error messages
 
